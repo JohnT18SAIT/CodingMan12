3bcb32c44827a40ece52f0d523b6bdd4a85f004a
John.Tarectecan <john.tarectecan@edu.sait.ca>
2023-10-17 01:23:37 -0400

    Added temp.py to repo

--- temp.py
+++ temp.py
@@ -1,3 +1,11 @@
-celsius = 25
-fahrenheit = (celsius * 9/5) + 32
-print (f"{} Celsius to Fahrenheit is {}")
+#celsius = 25
+#fahrenheit = (celsius * 9/5) + 32
+#print (f"{celsius} Celsius to Fahrenheit is {fahrenheit}")
+
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+celsius_temperature = 25
+fahrenheit_temperature = celsius_to_fahrenheit(celsius_temperature)
+print(f"{celsius_temperature} degrees Celsius is equal to {fahrenheit_temperature} degrees Fahrenheit")
